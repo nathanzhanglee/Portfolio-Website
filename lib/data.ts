b4b54c8b826fb7c4637e7e9614d7850b73d1133d
Nathan Lee <nathanzhanglee@gmail.com>
2025-01-26 17:03:24 -0500

    pandemic simulation project

--- lib/data.ts
+++ lib/data.ts
@@ -1,6 +1,7 @@
 import spotifyImg from "@/public/spotify.png";
 import flightImg from "@/public/flight-delay.png";
 import minesweeperImg from "@/public/minesweeper.png";
+import pandemicImg from "@/public/minesweeper.png";
 
 export const links = [
   {
@@ -31,6 +32,13 @@ export const links = [
 
 export const projectsData = [
   {
+    title: "Epidemiological Simulator",
+    description:
+      "Developed an interactive pandemic simulation tool to analyze infection spreads following SIR epidemiology model, with adjustable parameters like lockdowns and mask mandates. 1st Place in HoyaHacks 2024 Simulation Track",
+    tags: ["Python", "HTML", "CSS", "FastAPI"],
+    imageUrl: pandemicImg,
+  },
+  {
     title: "Spotify Clone",
     description:
       "Built a full-stack Spotify clone with a responsive UI and real-time data handling. Features include song uploads, playlist creation, and recurring Stripe subscriptions.",
@@ -47,7 +55,7 @@ export const projectsData = [
   {
     title: "Minesweeper",
     description:
-      "Developed Minesweeper with a GUI replicating Microsoft Window's version from the 90s, featuring save and reload function via file I/O operations.",
+      "Implemented Minesweeper with a GUI replicating Microsoft Window's version from the 90s, featuring save and reload function via file I/O operations.",
     tags: ["Java", "JUnit", "Java Swing"],
     imageUrl: minesweeperImg,
   },
